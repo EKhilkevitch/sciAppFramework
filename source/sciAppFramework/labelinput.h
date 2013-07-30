88f5f3e265d88c6431c5a2fbee1ea3bcfd7e1f53
EKhilkevitch <evgeniy-hila@yandex.ru>
2013-07-30 12:06:50 +0400

    labelInput backward compatibility file

--- source/sciAppFramework/labelinput.h
+++ source/sciAppFramework/labelinput.h
@@ -11,6 +11,15 @@
 using namespace sciAppFramework;
 
 typedef labelEditWidget labelEdit;
+typedef labelSpinWidget labelSpin;
+typedef labelPathEditWidget labelEditPath;
+typedef labelComboWidget labelCombo;
+
+#define LABELINPUT_SAVE_SETTINGS( Name ) \
+        do { (Name)->setSettingsName(#Name); (Name)->saveSettings( Settings ); } while (false)
+
+#define LABELINPUT_LOAD_SETTINGS( Name, Default ) \
+        do { (Name)->setSettingsName(#Name); (Name)->loadSettings( Settings ); } while (false)
 
 // ======================================================
 
