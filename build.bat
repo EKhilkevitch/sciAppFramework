cfb9645fa88f6d1be2c2506700057d981a730424
Evgeniy Khilkevitch <evgeniy-hila@yandex.ru>
2014-10-02 16:14:27 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,7 +5,6 @@ rem =========================================
 
 set ROOT_DIR=%~dp0
 set BUILD_DIR=build
-set CURRENT_DIR=%CD%
 
 rem =========================================
 
@@ -15,33 +14,27 @@ if not exist %BUILD_DIR% (
   mkdir %BUILD_DIR% || goto End
 )
 
-cd %BUILD_DIR% || goto End
+pushd %BUILD_DIR% || goto End
 cmake -G "NMake Makefiles" %1 %2 %3 %4 %5 %6 %7 %8 %9 .. || goto End
 nmake || goto End
+popd
 
 rem =========================================
 
 :End
 
 if %ERRORLEVEL% GTR 0 (
-  echo Build failure.
-  goto Exit
-)
-
+echo.
+echo Build failure.
+echo.
+popd
+exit /b 1
+) else (
 echo.
 echo Successfully build
 echo.
-goto Exit
-
-rem =========================================
-
-:Exit
-
-cd %CURRENT_DIR%
-
-if %ERRORLEVEL% GTR 0 (
-  exit /b 1 
-) else (
-  exit /b 0
+popd
+exit /b 0
 )
 
+rem =========================================
