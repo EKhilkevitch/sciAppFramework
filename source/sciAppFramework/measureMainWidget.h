f8e9900242c68ef8032e8b648ac261cd2f5e3a53
EKhilkevitch <evgeniy-hila@yandex.ru>
2015-04-15 14:50:05 +0300

    Small CMakeFile bug fixed

--- source/sciAppFramework/measureMainWidget.h
+++ source/sciAppFramework/measureMainWidget.h
@@ -19,6 +19,8 @@ namespace sciAppFramework
 
   class measureMainWidget : public mainWidget
   {
+    Q_OBJECT
+
     private:
       measurementThread *MeasurementThread;
       QTimer *MeasurementTimer;
